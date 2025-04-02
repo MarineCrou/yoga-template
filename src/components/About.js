634ec791b6abc1298dcb0c937275e33d81356fb1
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-04-02 19:00:40 +0200

    creating components for the main page

---
+++ src/components/About.js
@@ -0,0 +1,91 @@
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
+
+export default function About() {
+  return (
+    <div className="relative bg-orange-900 mt-32 sm:mt-56">
+      <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
+        <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
+          <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
+            <img
+              alt=""
+              src="https://plus.unsplash.com/premium_photo-1669446008800-9a124b0fd3a2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="absolute inset-0 size-full bg-stone-50 object-cover"
+            />
+          </div>
+        </div>
+        <div className="px-6 lg:contents">
+          <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
+            <p className="text-base/7 font-semibold text-stone-200">
+              Yoga Teacher
+            </p>
+            <h1 className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-orange-200 sm:text-5xl">
+              About Me
+            </h1>
+            <p className="mt-6 text-xl/8 text-stone-200">
+              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
+              At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at
+              vitae feugiat egestas ac. Diam nulla orci at in viverra
+              scelerisque eget. Eleifend egestas fringilla sapien.
+            </p>
+            <div className="mt-10 max-w-xl text-base/7 text-stone-200 lg:max-w-none">
+              <p>
+                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                Id dolor praesent donec est. Odio penatibus risus viverra tellus
+                varius sit neque erat velit. Faucibus commodo massa rhoncus,
+                volutpat. Dignissim sed eget risus enim. Mattis mauris semper
+                sed amet vitae sed turpis id.
+              </p>
+              <ul className="mt-8 space-y-8 text-stone-200">
+                <li className="flex gap-x-3">
+                  <CloudArrowUpIcon
+                    aria-hidden="true"
+                    className="mt-1 size-5 flex-none text-orange-200"
+                  />
+                  <span>
+                    <strong className="font-semibold text-orange-200">
+                      200h Yoga Teacher Training (Yoga Alliance)
+                    </strong>{" "}
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Maiores impedit perferendis suscipit eaque, iste dolor
+                    cupiditate blanditiis ratione.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <LockClosedIcon
+                    aria-hidden="true"
+                    className="mt-1 size-5 flex-none text-orange-200"
+                  />
+                  <span>
+                    <strong className="font-semibold text-orange-200">
+                      Trauma-informed
+                    </strong>{" "}
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                    qui lorem cupidatat commodo.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <ServerIcon
+                    aria-hidden="true"
+                    className="mt-1 size-5 flex-none text-orange-200"
+                  />
+                  <span>
+                    <strong className="font-semibold text-orange-200">
+                      15 years of yoga practice
+                    </strong>{" "}
+                    Ac tincidunt sapien vehicula erat auctor pellentesque
+                    rhoncus. Et magna sit morbi lobortis.
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
